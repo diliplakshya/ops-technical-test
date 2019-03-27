0cd36e2db4b282f043d8bf1ead326c2bc1dd26a6
Dilip Kumar <diliplakshya@gmail.com>
2019-03-27 21:29:59 +1100

    aded check in setting file

--- platform_enablement/settings.py
+++ platform_enablement/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
-import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -24,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '_5w_op-p_8c%j7ovh&2qu@i01uz9c#gq!za3@-ob5*nwqx5c5c'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
@@ -121,5 +120,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-# Activate Django-Heroku.
-django_heroku.settings(locals())
+if 'HEROKU' in os.environ:
+    import django_heroku
+    # Activate Django-Heroku.
+    django_heroku.settings(locals())
