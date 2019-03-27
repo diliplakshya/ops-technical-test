692fa16c6955ef9288f41f26aaba4390abdbe777
Dilip Kumar <diliplakshya@gmail.com>
2019-03-27 22:25:00 +1100

    added static directory

--- platform_enablement/settings.py
+++ platform_enablement/settings.py
@@ -121,14 +121,13 @@ if 'HEROKU' in os.environ:
     django_heroku.settings(locals())
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.11/howto/static-files/
-PROJECT_ROOT   =   os.path.join(os.path.abspath(__file__))
-STATIC_ROOT  =   os.path.join(PROJECT_ROOT, 'staticfiles')
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
-# Extra lookup directories for collectstatic to find static files
+# Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
+    os.path.join(BASE_DIR, 'static'),
 )
 
 #  Add configuration for static files storage using whitenoise
