54588ed6942aa530fade27aead1ad7115ace8c14
Dilip Kumar <diliplakshya@gmail.com>
2019-03-30 23:52:13 +1100

    Added str representation of models object

--- technicaltest/models.py
+++ technicaltest/models.py
@@ -3,8 +3,14 @@ from django.db import models
 class Home(models.Model):
     display_text = models.CharField(max_length=200)
 
+    def __str__(self):
+        return f'{self.display_text}'
+
 class MetaData(models.Model):
     version = models.CharField(max_length=10)
     description = models.TextField()
     last_commit_sha = models.CharField(max_length=50, blank=True)
     commit_message = models.CharField(max_length=200, default='Code Commit')
+
+    def __str__(self):
+        return f'{self.version}, {self.description}'
