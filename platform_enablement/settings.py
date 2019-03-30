4be47cb51d346550d3e7ee46608e661aa08cb7a3
Dilip Kumar <diliplakshya@gmail.com>
2019-03-30 16:45:13 +1100

    Debugging server errror

--- platform_enablement/settings.py
+++ platform_enablement/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '_5w_op-p_8c%j7ovh&2qu@i01uz9c#gq!za3@-ob5*nwqx5c5c'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'ops-technical-test.herokuapp.com']
 
